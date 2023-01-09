b433ffadf1d6cf979084ce8c2a2af36ba8572161
Amaka007 <real4amy@gmail..com>
2023-01-09 08:47:04 -0800

    sure

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
